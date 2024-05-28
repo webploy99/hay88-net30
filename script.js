4c48d6d35d8a35d599d4fba6e9c5e8a2a496f0f2
webploy99 <webploy99@gmail.com>
2024-05-28 09:29:16 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,73 @@
+// Show and hide menu
+
+    function OpenNav(){
+        document.getElementById("Nav").style.width = "100%";
+    }
+    function CloseNav(){
+        document.getElementById("Nav").style.width = "0%";
+    }
+
+    // url
+    function url() {
+        location.href="https://www.hay8833.com/?inviteCode=3128320";
+    }
+
+    // ==========================================
+    window.onscroll = function () { scrollFunction() };
+
+    function scrollFunction() {
+        if (document.documentElement.scrollTop > 80) {
+            document.getElementById("navbar").style.top = "0";
+        } else {
+            document.getElementById("navbar").style.top = "-128px";
+        }
+    }
+
+    // When the user clicks on the button, scroll to the top of the document
+    function topScroll() {
+        document.body.scrollTop = 0;
+        document.documentElement.scrollTop = 0;
+    }
+
+    // ===============splide slider=====================
+
+    var swiper = new Swiper(".mySwiper", {
+      spaceBetween: 20,
+      centeredSlides: true,
+      slidesPerView: 1,
+      // effect: "cube",
+      grabCursor: true,
+      loop: true,
+      autoplay: {
+        delay: 3000,
+        disableOnInteraction: false,
+      },
+      breakpoints: {
+          320: {
+          slidesPerView: 1,
+          },
+          480: {
+          slidesPerView: 2,
+          },
+      },
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+      cubeEffect: {
+        shadow: true,
+        slideShadows: true,
+        shadowOffset: 20,
+        shadowScale: 0.94,
+      },
+      on: {
+        autoplayTimeLeft(s, time, progress) {
+          const currentSlide = s.slides[s.activeIndex];
+          const progressBar = currentSlide.querySelector(".progressBar");
+          if (progressBar) {
+            const widthPercentage = parseFloat(progress) * 100;
+            progressBar.style.width = widthPercentage + "%";
+          }
+        },
+      },
+    });
